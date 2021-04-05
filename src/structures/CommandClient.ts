f5bb4a27d5a05fb7ab67c1781f6d1279b5c7718e
파랑이 <parangee9706@gmail.com>
2021-04-05 21:50:09 +0900

    feat: pass options to command client

--- src/structures/CommandClient.ts
+++ src/structures/CommandClient.ts
@@ -1,7 +1,8 @@
 import {Client, ClientOptions} from 'discord.js'
+import {CommandClientOptions} from "../types";
 
 export class CommandClient extends Client {
-    constructor(clientOptions: ClientOptions) {
+    constructor(clientOptions: ClientOptions, public commandOptions: CommandClientOptions) {
         super(clientOptions)
     }
 }
