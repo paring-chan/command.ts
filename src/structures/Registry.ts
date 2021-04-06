4d654bfc66a01a213a25e644c31cac43265f232b
파랑이 <parangee9706@gmail.com>
2021-04-06 17:20:31 +0900

    feat: use CommandManager in registry

--- src/structures/Registry.ts
+++ src/structures/Registry.ts
@@ -1,8 +1,10 @@
 import { Module } from './Module'
 import { Collection } from 'discord.js'
+import { CommandManager } from './CommandManager'
 
 export class Registry {
   modules: Collection<string, Module> = new Collection<string, Module>()
+  commandManager = new CommandManager()
 
   registerModule(id: string, module: Module) {
     this.modules.set(id, module)
