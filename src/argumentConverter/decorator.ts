77a99981e44e4eacaec588f33109ecc8960ff906
파랑이 <parangee9706@gmail.com>
2021-04-06 21:29:58 +0900

    feat: init converter decorator

---
+++ src/argumentConverter/decorator.ts
@@ -0,0 +1,19 @@
+import { Module } from '../structures'
+// import { IListener, ListenerDecorator } from '..'
+// import { LISTENERS_KEY } from '../constants'
+
+export function argConverter(event: string): MethodDecorator {
+  return (target, propertyKey) => {
+    if (!(target instanceof Module)) {
+      throw new TypeError('Class does not extend `Module` class.')
+    }
+    // const meta: ListenerDecorator = {
+    //   event,
+    //   id: target.constructor.name + ':' + (propertyKey as string),
+    //   key: propertyKey as string,
+    // }
+    // const metas: IListener[] = Reflect.getMetadata(LISTENERS_KEY, target) || []
+    // metas.push(meta)
+    // Reflect.defineMetadata(LISTENERS_KEY, metas, target)
+  }
+}
