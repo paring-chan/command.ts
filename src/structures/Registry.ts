cd6c9d4e5482e708d9f9555c2f46b4ae4e698b62
파랑이 <parangee9706@gmail.com>
2021-04-06 21:10:46 +0900

    feat: implement module unregister

--- src/structures/Registry.ts
+++ src/structures/Registry.ts
@@ -11,9 +11,22 @@ export class Registry {
   commandManager = new CommandManager()
   listenerManager = new ListenerManager(this.client)
 
-  registerModule(id: string, module: Module) {
+  async registerModule(id: string, module: Module) {
+    await module.load()
     this.modules.set(id, module)
     this.listenerManager.register(module)
     this.commandManager.register(module)
+  }
+
+  async unregisterModule(modOrID: string | Module) {
+    const key: string =
+      typeof modOrID === 'string'
+        ? modOrID
+        : this.modules.findKey((x) => x === modOrID)!
+    if (!key) throw new Error('Module not found or not registered.')
+    const module = this.modules.get(key)!
+    await module.unload()
+    this.listenerManager.unregister(module)
+    this.commandManager.unregister(module)
   }
 }
