1b5713da2c0c1c2151c774f9951a7d153b320eb0
파랑이 <parangee9706@gmail.com>
2021-04-12 22:22:32 +0900

    feat: auto generate api sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+function generateAPISidebar(dir = '') {
+  const p = path.join(__dirname, '../docs/api/' + dir)
+  const baseUrl = path.join('/docs/api', dir)
+  const result = []
+  const content = fs.readdirSync(p)
+  const directories = content.filter((x) =>
+    fs.lstatSync(path.join(p, x)).isDirectory(),
+  )
+  const files = content.filter(
+    (x) => !fs.lstatSync(path.join(p, x)).isDirectory(),
+  )
+  for (const directory of directories) {
+    result.push({
+      title: directory,
+      children: generateAPISidebar(directory),
+      path: path.join(
+        baseUrl,
+        directory,
+        fs.readdirSync(path.join(p, directory))[0].replace(/\.md$/, ''),
+      ),
+    })
+  }
+  for (const file of files) {
+    const name = file.replace(/\.md$/, '')
+    if (name === 'README') continue
+    result.push({
+      title: name,
+      path: path.join(baseUrl, name),
+    })
+  }
+  return result
+}
+
 module.exports = {
   title: 'Command.TS',
   description: require('../../package.json').description,
@@ -15,12 +50,7 @@ module.exports = {
       {
         title: 'API',
         path: '/docs/api/',
-        children: [
-          {
-            title: 'Modules',
-            path: '/docs/api/modules',
-          },
-        ],
+        children: generateAPISidebar(),
       },
     ],
   },
