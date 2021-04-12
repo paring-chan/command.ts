15d6f7a8ca4a31d602b60f65bfcc0cc57e22d2a9
파랑이 <parangee9706@gmail.com>
2021-04-12 21:27:26 +0900

    feat: edit button

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   title: 'Command.TS',
   description: require('../../package.json').description,
+  themeConfig: {
+    repo: 'pikokr/command.ts-v2',
+    docsRepo: 'pikokr/command.ts-v2',
+    docsBranch: 'master',
+    editLinks: true,
+    docsDir: 'docs',
+  },
 }
