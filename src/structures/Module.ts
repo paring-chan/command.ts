4190c07267f6428dc855da5a7e594dc99ea0faf1
파랑이 <parangee9706@gmail.com>
2021-09-08 18:20:35 +0900

    Update Module.ts

--- src/structures/Module.ts
+++ src/structures/Module.ts
@@ -1,4 +1,11 @@
+import { KCommands } from '../constants'
+import type { Command } from '../command'
+
 export abstract class Module {
+  get commands(): Command[] {
+    return Reflect.getMetadata(KCommands, this) || []
+  }
+
   load() {}
   unload() {}
   beforeReload() {}
