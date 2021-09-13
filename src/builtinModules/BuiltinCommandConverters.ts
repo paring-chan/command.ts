0ec41631aa9a9ac6e69a4f57c9773509d01f7953
파링 <parangee9706@gmail.com>
2021-09-13 23:17:17 +0900

    add builtin argument converters

--- src/builtinModules/BuiltinCommandConverters.ts
+++ src/builtinModules/BuiltinCommandConverters.ts
@@ -1,8 +1,16 @@
 import { BuiltInModule } from './BuiltInModule'
 import { argumentConverter } from '../command'
-import { Message } from 'discord.js'
+import { Client, GuildMember, Message, User } from 'discord.js'
+import { CommandClient } from '../structures'
 
 export class BuiltinCommandConverters extends BuiltInModule {
+  client: Client
+
+  constructor(private cts: CommandClient) {
+    super()
+    this.client = cts.client
+  }
+
   @argumentConverter(Message, false)
   message(msg: Message) {
     return msg
@@ -11,5 +19,38 @@ export class BuiltinCommandConverters extends BuiltInModule {
   @argumentConverter(String)
   string(msg: Message, arg: string) {
     return arg
+  }
+
+  getUserIDByMention(mention: string): `${bigint}` | undefined {
+    if (!mention) return
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+      mention = mention.slice(2, -1)
+      if (mention.startsWith('!')) {
+        mention = mention.slice(1)
+      }
+      return mention as `${bigint}`
+    }
+  }
+
+  @argumentConverter(User)
+  user(value: string): User | null {
+    const id = this.getUserIDByMention(value)
+    if (!id) return null
+    const user = this.client.users.cache.get(id)
+    return user || null
+  }
+
+  @argumentConverter(GuildMember)
+  member(value: string, msg: Message): GuildMember | undefined {
+    const id = this.getUserIDByMention(value)
+    if (!id) return
+    const user = msg.guild?.members.cache.get(id)
+    return user || undefined
+  }
+
+  @argumentConverter(Number)
+  number(value: string) {
+    const n = Number(value)
+    return isNaN(n) ? undefined : n
   }
 }
